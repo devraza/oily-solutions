10a975e9782e38188ba5da0074e9549c25d0f7f7
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-11-20 10:06:59 +0000

    solutions: 6

---
+++ sum_square_difference.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let mut total: u64 = 0;
+    let mut total1: u64 = 0;
+
+    for i in 1..=100 {
+        total += i*i; 
+        total1 += i;
+    }
+
+    let square_sum = total1.pow(2);
+    let difference = square_sum-total;
+
+    println!("{}", difference);
+}
