cd8e40d2caaf287ae48e47496d4d869fac5074f2
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-11-20 08:36:13 +0000

    solutions: 4

---
+++ largest_palindrome_product.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut product: i64 = 0; 
+
+    for i in 1..=999 {
+        for j in 1..=999 {
+            let product_string: String = format!("{:?}", i*j);
+            let reversed_string = product_string.chars().rev().collect::<String>();
+
+            if i*j > product && reversed_string == product_string {
+                product = i*j;
+            }
+        }
+    }
+    
+    println!("{}", product);
+}
