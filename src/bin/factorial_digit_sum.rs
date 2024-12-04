e058da0bd4cce4a39dee87c4ce078dcd57a2d1c3
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2024-12-04 10:41:04 +0000

    solutions: 20

---
+++ src/bin/factorial_digit_sum.rs
@@ -0,0 +1,19 @@
+use num_bigint::BigUint;
+
+fn factorial(x: u64) -> BigUint {
+    let mut product = BigUint::from(1 as u64);
+    for i in 2..=x {
+        product *= BigUint::from(i);
+    }
+    product.into()
+}
+
+fn main() {
+    let number = factorial(100);
+    let mut digits: Vec<u64> = vec![];
+    for i in number.to_string().chars().filter_map(|c| c.to_digit(10)) {
+        digits.push(i as u64);
+    }
+
+    println!("{}", digits.iter().sum::<u64>());
+}
